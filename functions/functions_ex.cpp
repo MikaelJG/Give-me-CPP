a51327e05ec52a58d51851893bbf110aad6a29f2
MikaelJG <mjgonsalves@protonmail.com>
2023-02-24 21:57:03 -0500

    functions and filesystem

---
+++ functions/functions_ex.cpp
@@ -0,0 +1,36 @@
+Certainly! Here are some more common built-in functions in C++:
+
+    isalpha() - checks if a character is an alphabet letter
+    isdigit() - checks if a character is a digit
+    isalnum() - checks if a character is an alphanumeric character
+    isspace() - checks if a character is a white-space character (e.g. space, tab, newline)
+    tolower() - converts a character to lowercase
+    toupper() - converts a character to uppercase
+    strcpy() - copies a string
+    strcat() - concatenates two strings
+    strlen() - returns the length of a string
+    strcmp() - compares two strings
+    strchr() - finds the first occurrence of a character in a string
+    strstr() - finds the first occurrence of a substring in a string
+    abs() - returns the absolute value of a number
+    ceil() - rounds a number up to the nearest integer
+    floor() - rounds a number down to the nearest integer
+    pow() - raises a number to a given power
+    sqrt() - returns the square root of a number
+    rand() - generates a random number
+
+There are many more built-in functions in C++, but these are some of the most commonly used ones.
+
+    cout and cin: for input and output operations.
+    strlen: to determine the length of a string.
+    strcmp: to compare two strings.
+    strcat: to concatenate two strings.
+    stoi: to convert a string to an integer.
+    stod: to convert a string to a double.
+    tolower and toupper: to convert a character to lowercase or uppercase.
+    abs: to get the absolute value of a number.
+    sqrt: to calculate the square root of a number.
+    pow: to raise a number to a certain power.
+    rand: to generate a random number.
+    time: to get the current time.
+    exit: to terminate the program.
