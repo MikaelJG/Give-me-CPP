760af4074149e3dadd5d4f46a8c8acd42e3d381c
MikaelJG <mjgonsalves@protonmail.com>
2023-06-09 15:46:43 -0400

    second project creation footsteps

--- project_creator.sh
+++ project_creator.sh
@@ -13,56 +13,67 @@
 #
 # Create project achitecture (tree)
 #
-# mkdir build include test src ;
+mkdir build include test src ;
 #
 # Create a CMakeList.txt file
 #
-# echo "
-# cmake_minimum_required(VERSION 3.10)
-# set(CMAKE_CXX_STANDARD 17)  
-# set(CMAKE_CXX_STANDARD_REQUIRED ON)  
-#
-# project(name-of-project VERSION 1.0)
-# 
-# target_include_directories(name-of-project PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/include)
-# # include is for header files (standard)
-#
-#
-#file(GLOB_RECURSE SRC_FILES src/*.cpp)
-## glob all .cpp files in src directory
-#
-# add_executable (name-of-project main.cpp ${SRC_FILES})
-## add all .cpp files in src to executable
-#
-# " >> CMakeList.txt
+echo "
+cmake_minimum_required(VERSION 3.10)
+set(CMAKE_CXX_STANDARD 17)  
+set(CMAKE_CXX_STANDARD_REQUIRED ON)  
+
+project(name-of-project VERSION 1.0)
+
+target_include_directories(name-of-project PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/include)
+# include is for header files (standard)
+
+
+file(GLOB_RECURSE SRC_FILES src/*.cpp)
+# glob all .cpp files in src directory
+
+add_executable (name-of-project main.cpp ${SRC_FILES})
+# add all .cpp files in src to executable
+
+" >> CMakeLists.txt
 #
 # Create a header file in the include directory
 #
-# echo "
-# #pragma once
-# #include <iostream>
+echo "
+#pragma once
+#include <iostream>
+
+class Blah {
+  public:
+      void boo();
+}; " >> ./include/Blah.h
+# Declarations here, in header file Blah.h
+
 #
-# class Blah {
-#   public:
-#       void boo();
-# }; " >> ./include/Blah.h
-# # Declarations here, in header file Blah.h
+# Create a source file for the class Blah in the src directory
 #
-# echo "
-# #include "blah.h"
-# #include <iostream>
-# void Blah::boo() {
-#   std::cout << "This Blah.cpp" << '\n';
-# }
+echo ' 
+#include "blah.h"
+#include <iostream>
+void Blah::boo() {
+  std::cout << "This Blah.cpp" << "\n";
+} ' >> ./src/Blah.cpp
+# Declared in header file Blah.h
+# Defined here in blah source file, Blah.cpp
+
 #
+# Create a main.cpp src directory
 #
-# " >> ./src/Blah.cpp
-# # Declared in header file Blah.h
-# # Defined here in blah source file, Blah.cpp
-#
-#
+echo ' 
+#include <iostream>
+int main() {
+    std::cout << "This main.cpp" << "\n"; 
+    return 0;
+}void Blah::boo() {
+  std::cout << "This Blah.cpp" << "\n";
+} ' >> ./src/main.cpp
+
 # Run cmake from the cmd line, specify a directory
-#
+# 
 # cmake . && make && ./hello
 #
 # 
