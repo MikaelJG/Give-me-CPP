5ee709f0d6bfd85bd6d7eaf08f07115b9d764b9e
MikaelJG <mjgonsalves@protonmail.com>
2023-06-09 15:21:01 -0400

    first header file

--- project_creator.sh
+++ project_creator.sh
@@ -11,6 +11,10 @@
 #
 ################################################################ 
 #
+# Create project achitecture (tree)
+#
+# mkdir build include test ;
+#
 # Create a CMakeList.txt file
 #
 # echo "
@@ -19,6 +23,35 @@
 # set(CMAKE_CXX_STANDARD_REQUIRED ON)  
 #
 # project(name-of-project VERSION 1.0)
-# add_executable (name-of-project main.cpp)"
+# add_executable (name-of-project main.cpp)
+# 
+# target_include_directories(name-of-project PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/include)
+# # include is for header files (standard)
 #
 #
+# " >> CMakeList.txt
+#
+# Create a header file in the include directory
+#
+# echo "
+# #pragma once
+# #include <iostream>
+#
+# class Blah {
+#   public:
+#       inline void boo() {
+#           std::cout << "This Blah.h" << <'\n';
+#        
+#       }
+#
+#
+# };
+#
+#
+# " >> Blah.h
+#
+# Run cmake from the cmd line, specify a directory
+#
+# cmake . && make && ./hello
+#
+# 
