5baa5c15aea55a6c5f2b64892ad624958a8649ce
MikaelJG <mjgonsalves@protonmail.com>
2023-06-09 15:11:17 -0400

    create a cmakelist

--- project_creator.sh
+++ project_creator.sh
@@ -1,5 +1,24 @@
 #
 # compile and run automatically (with one command)
+#
+## first, cmake generates make files
+## second, make runs the make files & creates an executable
+## ./hello, run the executable 
+#
+#
 # test automatically (with one command)
 # build automatically (get all dependencies)
+#
+################################################################ 
+#
+# Create a CMakeList.txt file
+#
+# echo "
+# cmake_minimum_required(VERSION 3.10)
+# set(CMAKE_CXX_STANDARD 17)  
+# set(CMAKE_CXX_STANDARD_REQUIRED ON)  
+#
+# project(name-of-project VERSION 1.0)
+# add_executable (name-of-project main.cpp)"
+#
 #
