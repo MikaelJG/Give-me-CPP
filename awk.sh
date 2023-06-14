3f8dafc3562a064da97f70b20c9570b069dcf65c
MikaelJG <mjgonsalves@protonmail.com>
2023-06-14 17:15:19 -0400

    lets go

--- awk.sh
+++ awk.sh
@@ -1,1 +1,37 @@
-awk '/verbat/ { print NR ": " $0 }' 3_cpp.tex
+awk '/begin{verbat/ { print NR }' 3_cpp.tex >> begin.txt
+awk '/end{verbat/ { print NR }' 3_cpp.tex >> end.txt
+
+
+# find the number of lines in begin 
+#
+# 
+#
+#
+# find the number of lines in end
+#
+#
+#
+# Save it in a value
+#
+#
+# for (i = 0; i < end_number_of_lines.size(); i++) {
+       # head -n i file.txt 
+#
+# }
+#
+#
+#
+#
+# find the number of the first begin - first end
+#
+# for every line do
+
+
+#
+
+cat end.txt ; cat begin.txt
+
+
+
+
+
