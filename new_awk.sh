8d26b58f945e9776b9cb9c5b9af3f976de50b484
MikaelJG <mjgonsalves@protonmail.com>
2023-06-14 20:36:22 -0400

    getting closer

--- new_awk.sh
+++ new_awk.sh
@@ -1,46 +1,37 @@
 #!/bin/bash
 
-rm begin.txt end.txt
+rm begin.txt end.txt both.csv sub.txt
 
 awk '/begin{verbat/ { print NR }' 3_cpp.tex >> begin.txt
 awk '/end{verbat/ { print NR }' 3_cpp.tex >> end.txt
 
 file1="begin.txt"
 file2="end.txt"
-output="output.csv"
+both="both.csv"
 
 # Combine the lines from both files into the output CSV file
-paste -d ',' "$file1" "$file2" > "$output"
+paste -d ',' "$file1" "$file2" > "$both"
 
-# Find the number of lines in begin.txt
-# BEGIN_LINES=$(wc -l < begin.txt)
-# echo "begin.txt has $BEGIN_LINES number of lines"
-# 
-# # Find the number of lines in end.txt
-# END_LINES=$(wc -l < end.txt)
-# echo "end.txt has $END_LINES number of lines"
-# 
-# for ((i = 1; i <= END_LINES; i++)); do
-# 
-#     read -r begin_value < begin.txt 
-#     read -r end_value < end.txt 
-#     
-#     begin_val_int=$((begin_value))
-#     end_val_int=$((end_value))
-#     sub=$((end_val_int - begin_val_int)) 
-#     
-#     echo "end: ${end_val_int} - begin ${begin_val_int}"
-#     echo "substraction is ${sub}"
-# 
-# done
+# begin, end, substracted
+# 335,358,23
+awk -F ',' '{result = $2 - $1; print $0 "," result}' both.csv >> sub.txt
 
+# print the range with sed command
+#3
+while IFS= read -r line; do
+    IFS=',' read -r var1 var2 var3 <<< "$line"
 
-# create a csv
+    int1=$((var1))
+    int2=$((var2))
+    int3=$((var3))
+
+    echo "int1: $int1"
+    echo "int2: $int2"
+    echo "int3: $int3"
+    echo "Line: $line"
+done < sub.txt
+
+# sed -n '335,350p' 3_cpp.tex
 #
-# begin_1, end_1
-# begin_2, end_2
-# begin_3, end_3
-#
-#
-#
-#
+# 
+
