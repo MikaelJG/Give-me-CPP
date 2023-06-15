f0ad3dd1d330c53876b3615ad0aa6ab4081299a9
MikaelJG <mjgonsalves@protonmail.com>
2023-06-15 11:04:07 -0400

    closer to section

--- new_awk.sh
+++ new_awk.sh
@@ -5,25 +5,24 @@ rm result.txt section.csv
 section_array() {
     sec_file="section.csv"
     section_array=()
+    chapter_name_array=()
     
     while IFS= read -r line; do
-      section_array+=("$line")
+        IFS=',' read -r section_num chapter_name <<< "$line"
+        section_array+=("$section_num")
+        chapter_name_array+=("$chapter_name")
     done < "$sec_file"
 
     length=${#section_array[@]}
     sqrtLength=$(echo "sqrt($length)" | bc)
     jmpAmount=$(echo "($sqrtLength + 0.5) / 1" | bc)
 
-    # print to see all values
-    echo "${section_array[@]}"
-    echo "this array has a length of: $length"
-    echo "It has a sqrt length of: $sqrtLength"
-    echo "The jmpAmount has been calculated at: $jmpAmount"
-
     i=$jmpAmount
 
+    target=$1
+
     while (( i < length )); do
-        if (( section_array[i] > 1230 )); then
+        if (( section_array[i] > target )); then
             break
         fi
         i=$((i + jmpAmount))
@@ -33,13 +32,15 @@ section_array() {
 
     local j
     for (( j = 0; j < jmpAmount && i < length; j++, i++ )); do
-        if (( section_array[i] > 1230 )); then
+        if (( section_array[i] > target )); then
             
             i=$(( i - 1 ))
 
-            echo "The number given is 1230 !"
+            echo "The number given is $1 !"
             echo "Section_array[i] is this! :: ${section_array[i]}"
-            echo $i
+            echo "Chapter name is: ${chapter_name_array[i]}"
+            
+            sleep 1
             return
         fi
     done
@@ -50,7 +51,7 @@ section_array() {
 
 awk '/begin{verbat/ { print NR }' 3_cpp.tex >> begin.txt
 awk '/end{verbat/ { print NR }' 3_cpp.tex >> end.txt
-awk '/section/ { print NR }' 3_cpp.tex >> section.csv
+awk '/section/ { print NR "," $0 }' 3_cpp.tex >> section.csv
 
 file1="begin.txt"
 file2="end.txt"
@@ -63,24 +64,26 @@ paste -d ',' "$file1" "$file2" > "$both"
 # 335,358,23
 awk -F ',' '{result = $2 - $1; print $0 "," result}' both.csv >> sub.txt
 
-section_array
 
-sleep 20
-
-# print the range with sed command
-#3
 while IFS= read -r line; do
     IFS=',' read -r begin_int end_int sub_int <<< "$line"
 
     b_int=$((begin_int))
     e_int=$((end_int))
     # s_int=$((sub_int))
+    #
+    echo "this is b_int : $b_int"
+    echo "this is e_int : $e_int"
 
-    sed -n "${b_int},${e_int}p" 3_cpp.tex
+    section_array $b_int
+
+    sleep 1 
+
+    # sed -n "${b_int},${e_int}p" 3_cpp.tex
     #
     # get rid of the begin{verba and end{verba line.
     # 
     
-done < sub.txt > result.txt
+done < sub.txt
 
 rm begin.txt end.txt both.csv sub.txt
