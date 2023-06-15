cb0b5ad3e2f3debbb046d826bc430a9a2030db25
MikaelJG <mjgonsalves@protonmail.com>
2023-06-14 20:57:02 -0400

    now all verba in one file

--- new_awk.sh
+++ new_awk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm begin.txt end.txt both.csv sub.txt
+rm result.txt
 
 awk '/begin{verbat/ { print NR }' 3_cpp.tex >> begin.txt
 awk '/end{verbat/ { print NR }' 3_cpp.tex >> end.txt
@@ -19,19 +19,16 @@ awk -F ',' '{result = $2 - $1; print $0 "," result}' both.csv >> sub.txt
 # print the range with sed command
 #3
 while IFS= read -r line; do
-    IFS=',' read -r var1 var2 var3 <<< "$line"
+    IFS=',' read -r begin_int end_int sub_int <<< "$line"
 
-    int1=$((var1))
-    int2=$((var2))
-    int3=$((var3))
+    b_int=$((begin_int))
+    e_int=$((end_int))
+    # s_int=$((sub_int))
 
-    echo "int1: $int1"
-    echo "int2: $int2"
-    echo "int3: $int3"
-    echo "Line: $line"
-done < sub.txt
+    sed -n "${b_int},${e_int}p" 3_cpp.tex
+done < sub.txt > result.txt
 
-# sed -n '335,350p' 3_cpp.tex
+rm begin.txt end.txt both.csv sub.txt
 #
 # 
 
