24d57f76f3e17e6712294762a73b2ff6742d92ed
MikaelJG <mjgonsalves@protonmail.com>
2023-07-09 18:09:25 -0400

    cpp workbook coming together

--- workbook/cp_fuze.sh
+++ workbook/cp_fuze.sh
@@ -3,23 +3,22 @@
 # pretty date
 date=$(date +"%m-%d-%H:%M")
 
-
 # relative paths 
 script_dir="$(dirname ${BASH_SOURCE[0]})"
 
-# get tools, to compile with cpp lang
 # the wildcard character * is missed when in quotes
-#
-cp ${script_dir}/../../assets/*.png "${script_dir}"
-cp "${script_dir}/../../7_tools/7_tools.tex" "${script_dir}"
-cp "${script_dir}/../../4_computer_science_algorithms/algorithms.tex" "${script_dir}"
-
 # cp assets, to compile with images
+
+cp ${script_dir}/../assets/*.png "${script_dir}"
+cp ${script_dir}/1_cpp/*.tex "${script_dir}"
+cp ${script_dir}/2_data_structure/*.tex "${script_dir}"
+cp ${script_dir}/3_cmake/*.tex "${script_dir}"
+cp ${script_dir}/4_gdb/*.tex "${script_dir}"
 
 path_1=${script_dir}
 files=$(ls $path_1)
-
-
+ 
+ 
 echo "
 \documentclass[openany]{report}
 \usepackage[margin=1.2in]{geometry}
@@ -32,7 +31,7 @@ echo "
 \maketitle
 \tableofcontents
 " > cpp_$date.tex
-
+# 
 for file in $files; do
 
     ext="${file##*.}"
@@ -48,9 +47,8 @@ for i in {1..3}; do
     pdflatex cpp_$date.tex
 done
 
-rm "${script_dir}/7_tools.tex" ; rm "${script_dir}/algorithms.tex"
-
-rm *.png 
+rm *.png ; 
 rm *.aux ; rm *.log ; rm *.toc
 mv *.pdf complete ; mv cpp_$date.tex complete
+rm *.tex 
 
