58b9d34e0124078e8998d0c4a4698878af2826b0
MikaelJG <mjgonsalves@protonmail.com>
2023-07-13 20:54:06 -0400

    short cmake

---
+++ workbook/3_cmake/cmake_steps.sh
@@ -0,0 +1,12 @@
+# Create source file and CMakeLists.txt
+#
+# mkdir build directory
+#
+# cd build dir.
+#
+# cmake .. ---- specify that the source file is in parent directory
+#           --- Generates the Build Files / Configure the project
+#
+# cmake --build .  ---- creates the executable
+#
+# ./Executable , run the executable
