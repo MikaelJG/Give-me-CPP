79b48939c4b0726b83a042cf5f89ccf04d78b71b
MikaelJG <mjgonsalves@protonmail.com>
2023-07-23 19:47:42 -0400

    finding files

--- workbook/textract_cp.sh
+++ workbook/textract_cp.sh
@@ -3,27 +3,24 @@
 script_dir="$(dirname ${BASH_SOURCE[0]})"
 
 echo $script_dir
-cpp_tex_file="${script_dir}/1_cpp"
-data_struct_tex_file="${script_dir}/2_data_structure"
-cmake_tex_file="${script_dir}/3_cmake"
-gdb_tex_file="${script_dir}/4_gdb"
+cpp_tex_file="${script_dir}/1_cpp/1_cpp.tex"
+data_struct_tex_file="${script_dir}/2_data_structure/2_data_structure.tex"
+cmake_tex_file="${script_dir}/3_cmake/3_cmake.tex"
+gdb_tex_file="${script_dir}/4_gdb/4_gdb.tex"
 
-if cd "$cpp_tex_file"; then
-    echo "cpp dir found"
+if cat "$cpp_tex_file" >/dev/null 2>&1; then
+    echo "cpp file found"
 fi
 
-cd ..
-
-if cd "$data_struct_tex_file"; then
-    echo "data dir found"
+if cat "$data_struct_tex_file" >/dev/null 2>&1; then
+    echo "data file found"
 fi
-cd ..
-if cd "$cmake_tex_file"; then
-    echo "cmake dir found"
-fi
-cd ..
 
-if cd "$gdb_tex_file"; then
-    echo "gdb dir found"
+if cat "$cmake_tex_file" >/dev/null 2>&1; then
+    echo "cmake file found"
+fi
+
+if cat "$gdb_tex_file" >/dev/null 2>&1; then
+    echo "gdb file found"
 fi
 # textra 
